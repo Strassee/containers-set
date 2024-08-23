3760b82ffcf4cfbfd707b21f3ce68d2eee507df3
Evgeniy Filkin <jeka_krsk@mail.ru>
2024-08-23 14:21:57 +0300

    Commit v 1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# containers-set
+
+[![Build status](https://ci.appveyor.com/api/projects/status/gd1qdr8l52jj3m0o?svg=true)](https://ci.appveyor.com/project/Strassee/containers-set)
---
+++ src/js/classes/Team.js
@@ -0,0 +1,22 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(hero) {
+    if (this.members.has(hero)) {
+      throw new Error('This hero is already in the team');
+    }
+    this.members.add(hero);
+  }
+
+  addAll(...heroes) {
+    heroes.forEach((hero) => {
+      this.members.add(hero);
+    });
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+}
